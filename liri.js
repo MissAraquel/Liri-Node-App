749972802885c0436e478c1fa7e3486fa055bd62
Miss Araquel <missaraquel@gmail.com>
2018-08-10 18:02:26 -0700

    Added module requirements, call for Twitter and Spotify, and variable for first argument

--- liri.js
+++ liri.js
@@ -1,2 +1,15 @@
+//Module requirements
 require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+
+//Twitter and Spotify info from  keys.js
+var client = new Twitter(keys.twitterKeys);
+var spotify = new Spotify(keys.spotify);
+
+//Arguments to variables
+var command = process.argv[2]
 
